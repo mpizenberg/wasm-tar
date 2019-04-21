3542d14781570b28d50b3bc34db3166aa56743dd
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-04-21 11:21:23 +0200

    Clean working code

--- index.js
+++ index.js
@@ -16,23 +16,16 @@ async function run() {
 
 function loadInput(input) {
   const tar_file = input.files[0];
-  console.log("File name: " + tar_file.name);
-  console.log("File size: " + tar_file.size);
-  // Ideally directly read into the wasm memory buffer!
-  // But I think that is not possible.
   file_reader.readAsArrayBuffer(tar_file);
 }
 
 // Transfer archive data to wasm when the file is loaded.
 function transferContent(arrayBuffer, wasm_tar, wasm) {
-  console.log("transferContent");
-  const size_allocated = wasm_tar.allocate(arrayBuffer.byteLength);
-  console.log("Size allocated: " + size_allocated);
+  wasm_tar.allocate(arrayBuffer.byteLength);
   const wasm_buffer = new Uint8Array(wasm.memory.buffer);
-  // Ideally, we should avoid the copy but well ...
-  wasm_buffer.set(arrayBuffer, wasm_tar.memory_pos());
-  // TODO: trigger wasm processing.
-  console.log("We got here!");
+  const start = wasm_tar.memory_pos();
+  const file_buffer = new Uint8Array(arrayBuffer);
+  wasm_buffer.set(file_buffer, start);
   console.log(wasm_tar.list_entries());
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,8 @@ impl WasmTar {
         }
     }
 
-    pub fn allocate(&mut self, length: usize) -> usize {
+    pub fn allocate(&mut self, length: usize) {
         self.tar_buffer = vec![0; length];
-        self.tar_buffer.len()
     }
 
     pub fn memory_pos(&self) -> *const u8 {
@@ -30,20 +29,16 @@ impl WasmTar {
 
         // Init a string containing all tar entries.
         let mut entries_string = Vec::new();
-        entries_string.push(format!("tar_buffer size: {}", self.tar_buffer.len()));
         entries_string.push("All entries:".to_string());
 
         // Iterate over all entries of the archive.
-        let mut nb_entries = 0;
         for file in archive.entries().unwrap() {
-            nb_entries += 1;
             // Check for an I/O error.
             let file = file.unwrap();
             entries_string.push(file.header().path().unwrap().to_str().unwrap().to_owned());
         }
 
         // Concatenate entries into one string.
-        entries_string.push(format!("nb_entries: {}", nb_entries));
         entries_string.join("\n")
     }
 }
